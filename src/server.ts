acdf214490a09468c96d337f4894f88702af6aba
renanaribeiro91 <renanadmribeiro@gmail.com>
2022-04-28 21:34:31 -0300

    organizando estrutura

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import 'dotenv/config'
-import express from 'express'
-import { router } from './routes/router'
+import "dotenv/config";
+import express from "express";
+// import { router } from './routes/router'
 
-const app = express()
+const app = express();
 
+const port = 8080;
+app.listen(port, () => {
+  console.log("server listening on port 8080");
+});
