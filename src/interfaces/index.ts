65cfc4a73b3b9777d8368fc07b91d2e7e19ea3e9
renanaribeiro91 <renanadmribeiro@gmail.com>
2022-05-02 22:47:15 -0300

    criando userRepo

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,3 +5,23 @@ export enum Categories {
   Saúde = "Saúde",
   Transporte = "Transporte",
 }
+export interface IUserRequest {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  cod_receita: number;
+  cod_despesa: number;
+}
+
+export interface IExpensesRequest {
+  id: string;
+  cod_despesas: number;
+  categories?: string;
+}
+
+export interface IRecipesRequest {
+  id: string;
+  cod_receitas: number;
+  desc_receitas: string;
+}
---
+++ src/repository/user/index.ts
@@ -0,0 +1,27 @@
+import { AppDataSource } from "../../db/dataSource";
+import { User } from "../../model/entities/User";
+import { IUserRequest } from "../../interfaces";
+
+const userRepo = AppDataSource.getRepository(User);
+
+export const findAllUser = async (): Promise<User[]> => {
+  return await userRepo.find();
+};
+
+export const findOneUser = async (idUser: string): Promise<User> => {
+  return await userRepo.findOneBy(idUser);
+};
+
+export const createUser = async (User: IUserRequest): Promise<User> => {
+  return await userRepo.create(users);
+};
+
+export const updateUser = async (idUser: string): Promise<User> => {
+  const usersUpdate = await userRepo.findOne(idUser);
+  return await userRepo.remove(usersUpdate);
+};
+
+export const removeUser = async (idUser: string): Promise<User> => {
+  const usersRemove = await userRepo.findOne(idUser);
+  return await userRepo.save(usersRemove);
+};
